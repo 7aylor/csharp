06f6714e4c672538519a264702fb7af0b3721e8e
Taylor <tbuchhei@gmail.com>
2016-10-22 16:47:55 -0700

    finished ch 34 lines of code

---
+++ The C# Player's Guide Second Edition Try It Out/ch34- Lines of Code.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinesOfCode
+{
+    public static class StringExtension
+    {
+        public static int LineCount(this string s)
+        {
+            //split string by newline
+            string[] lines = s.Split('\n');
+
+            //counter of blank lines
+            int blankCount = 0;
+
+            //loop through lines
+            for(int i = 0; i < lines.Length; i++)
+            {
+                //replace space, tab, and new lines charactes with ""
+                lines[i] = lines[i].Replace(" ", "");
+                lines[i] = lines[i].Replace("\t", "");
+                lines[i] = lines[i].Replace("\n", "");
+
+                //increase number of blank lines if line is ""
+                if (lines[i].Equals(""))
+                {
+                    blankCount++;
+                }
+            }
+
+            //return length of array minus blank lines
+            return lines.Length - blankCount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read file path, then print count of lines
+            Console.WriteLine("*****Lines of Code Counter*****\n");
+            Console.Write("Filepath: ");
+            string filePath = Console.ReadLine(); 
+            string file = File.ReadAllText(filePath);
+            
+            Console.ReadKey();
+        }
+    }
+}
