55e84deca8a099fb74744c68c6330a7e9c86a381
Taylor <tbuchhei@gmail.com>
2017-01-30 20:54:07 -0800

    Number wizard, first script in Udemy course

---
+++ Unity/Number Wizard/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Welcome to Number Wizard!");
+        print("Pick a number in your head, but don't tell me!");
+
+        int max = 1000;
+        int min = 1;
+
+        print("The highest number you can pick is " + max);
+        print("The lowest number you can pick is " + min);
+
+        print("Is the number higher or lower than 500?");
+        print("Up = higher, down = lower, return = equal.");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up arrow pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down arrow pressed");
+        }
+
+    }
+}
