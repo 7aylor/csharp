715249cf11c0e66ebe8497c1bdfc5f5251184aa8
Taylor <tbuchhei@gmail.com>
2017-02-16 22:14:53 -0800

    Early stages of console game with Rett

---
+++ Coding with Rett/Dungeon Crawler/Hero.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    class Hero
+    {
+        //int strength;
+        //int intelligence;
+        int health;
+        int speed; //helps determine who goes first in a battle
+        int attack;
+        int defense;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="health"></param>
+        /// <param name="speed"></param>
+        /// <param name="attack"></param>
+        /// <param name="defense"></param>
+        /// 
+        public Hero(int health, int speed, int attack, int defense)
+        {
+            //this.strength = strength;
+            //this.intelligence = intelligence;
+            this.health = health;
+            this.speed = speed;
+            this.attack = attack;
+            this.defense = defense;
+        }
+
+        public int Health
+        {
+            get
+            {
+                return this.health;
+            }
+            set
+            {
+                this.health = value;
+            }
+        }
+
+        public int Speed
+        {
+            get;
+            set;
+        }
+
+        public int Attack
+        {
+            get;
+            set;
+        }
+
+        public int Defense
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="enemy"></param>
+        public void inflictDamage(Hero enemy)
+        {
+            enemy.health -= this.attack;
+        }
+    }
+}
---
+++ Coding with Rett/Dungeon Crawler/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+
+
+            Console.WriteLine("\t\t\tWelcome to Dungeon Crawler!!\n\n");
+
+            Console.Write("You see a gate in front of you, walk through? (y/n): ");
+            choice = Console.ReadKey().KeyChar;
+
+            while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N')
+            {
+                Console.WriteLine();
+                Console.Write("Invalid input, please try again: ");
+                choice = Console.ReadKey().KeyChar;
+            }
+
+            if(choice == 'y' || choice == 'Y')
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("\nWell, thanks for playing. Bye!");
+                Console.ReadKey();
+                return;
+            }
+
+            Hero player = new Hero(100, 8, 5, 3);
+            Hero enemy = new Hero(20, 4, 2, 1);
+            
+            Console.WriteLine("\nEnemy's health before Attack: " + enemy.Health);
+            player.inflictDamage(enemy);
+            Console.WriteLine("\nEnemy's health after Attack: " + enemy.Health);
+
+
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
