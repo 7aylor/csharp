13ebfdec091bda32c276d9bf3b41c2991f51867f
Taylor <tbuchhei@gmail.com>
2017-02-17 13:19:24 -0800

    Day 2 of coding with Rett

--- Coding with Rett/Dungeon Crawler/Hero.cs
+++ Coding with Rett/Dungeon Crawler/Hero.cs
@@ -8,31 +8,42 @@ namespace DungeonCrawler
 {
     class Hero
     {
+        //instance variables
+        #region
         //int strength;
         //int intelligence;
         int health;
         int speed; //helps determine who goes first in a battle
         int attack;
-        int defense;
-        
+        String name;
+        Random rand = new Random();
+        List<Item> inventory = new List<Item>();
+        int inventoryCount = 0;
+        #endregion
+
+        //constructors
+        #region
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="health"></param>
         /// <param name="speed"></param>
         /// <param name="attack"></param>
-        /// <param name="defense"></param>
+        /// <param name="name"></param>
         /// 
-        public Hero(int health, int speed, int attack, int defense)
+        public Hero(int health, int speed, int attack, String name)
         {
             //this.strength = strength;
             //this.intelligence = intelligence;
             this.health = health;
             this.speed = speed;
             this.attack = attack;
-            this.defense = defense;
+            this.name = name;
         }
+        #endregion
 
+        //access modifiers (getters and setter)
+        #region
         public int Health
         {
             get
@@ -47,29 +58,182 @@ namespace DungeonCrawler
 
         public int Speed
         {
-            get;
-            set;
+            get
+            {
+                return this.speed;
+            }
+            set
+            {
+                this.speed = value;
+            }
         }
 
         public int Attack
         {
-            get;
-            set;
+            get
+            {
+                return this.attack;
+            }
+            set
+            {
+                this.attack = value;
+            }
         }
 
-        public int Defense
+        public String Name
         {
-            get;
-            set;
+            get
+            {
+                return this.name;
+            }
         }
+        #endregion
 
+        //functions
+        #region
         /// <summary>
         /// 
         /// </summary>
         /// <param name="enemy"></param>
-        public void inflictDamage(Hero enemy)
+        public int inflictDamage(Hero enemy)
         {
-            enemy.health -= this.attack;
+
+            float hitChance = (rand.Next(0, 11)) / 10f;
+
+            if(hitChance > 0.1)
+            {
+                int attackModifier = rand.Next(0, attack + 1);
+                enemy.health -= attackModifier;
+                if(enemy.health < 0)
+                {
+                    enemy.health = 0;
+                }
+                return attackModifier;
+            }
+            else
+            {
+                return 0;
+            }
         }
+
+        public void printInventory()
+        {
+            if(this.inventory.Count == 0)
+            {
+                Console.WriteLine("Your inventory is empty\n");
+            }
+            else
+            {
+                Console.WriteLine("Items in Inventory");
+
+                /*
+                foreach (Item item in this.inventory)
+                {
+                    Console.WriteLine("\tItem: " + item.Name);
+                    Console.WriteLine("\tType: " + item.Type + "\n");
+
+                    
+                    if(item.GetType() == typeof(Potion))
+                    {
+                        Console.WriteLine("Potion Size: " item);
+                    }
+                    if(item.GetType() == typeof(Weapon))
+                    {
+                        
+                    }
+                    
+                }*/
+                Console.WriteLine();
+            }
+        }
+
+        public void addItemToInvetory(Item item)
+        {
+            if(inventoryCount >= 5)
+            {
+                Console.WriteLine("Inventory is full.");
+            }
+            else
+            {
+                this.inventory.Add(item);
+                inventoryCount++;
+            }
+        }
+        #endregion
+    }
+
+    abstract class Item
+    {
+        protected string name;
+        protected string type;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return this.type;
+            }
+        }
+    }
+
+    class Weapon : Item
+    {
+        int damage;
+
+        /// <summary>
+        /// weapon constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <param name="damage"></param>
+        public Weapon(string name, string type, int damage)
+        {
+            this.name = name;
+            this.type = type;
+            this.damage = damage;
+        }
+
+        public int Damage
+        {
+            get
+            {
+                return this.damage;
+            }
+        }
+
+    }
+
+    class Potion : Item
+    {
+        int size;
+        /// <summary>
+        /// Potion constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <param name="size"></param>
+        public Potion(string name, string type, int size)
+        {
+            this.name = name;
+            this.type = type;
+            this.size = size;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return this.size;
+            }
+        }
+
     }
 }
--- Coding with Rett/Dungeon Crawler/Program.cs
+++ Coding with Rett/Dungeon Crawler/Program.cs
@@ -10,14 +10,171 @@ namespace DungeonCrawler
     {
         static void Main(string[] args)
         {
+
+            //playGame();
+
+            Weapon sword = new Weapon("Longclaw", "Sword", 5);
+            Potion healthPotion = new Potion("Small Health Potion", "Health Potion", 3);
+
+            Hero player = new Hero(100, 5, 5, "player");
+
+            //player.printInventory();
+
+            //player.addItemToInvetory(sword);
+            //player.printInventory();
+
+            //player.addItemToInvetory(healthPotion);
+            //player.printInventory();
+
+            Console.WriteLine(sword.GetType());
+            Console.WriteLine(healthPotion.GetType());
+
+            Console.ReadKey();
+        }
+
+        static void playGame()
+        {
             char choice;
-
-
+            Hero player = new Hero(100, 8, 5, "Player");
             Console.WriteLine("\t\t\tWelcome to Dungeon Crawler!!\n\n");
 
             Console.Write("You see a gate in front of you, walk through? (y/n): ");
             choice = Console.ReadKey().KeyChar;
 
+            choice = checkChar(choice);
+
+            if (choice == 'y' || choice == 'Y')
+            {
+                fightGorlack(player);
+            }
+            else
+            {
+                Console.WriteLine("\nWell, thanks for playing. Bye!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+
+        static void fightGorlack(Hero player)
+        {
+            Hero Gorlack = new Hero(20, 4, 2, "Gorlack the Weak");
+            Console.Clear();
+            Console.WriteLine("\nAs you walk through the gate, A menacing figure emerges from the shadows.\n");
+            Console.WriteLine("You recognize the hideous face of Gorlack the Weak.\n\n");
+            Console.Write("\"You've gone far enough. Prepare to meet your death! ");
+
+            char choice;
+
+            Console.WriteLine("Fight or flee? (y/n)");
+
+            choice = Console.ReadKey().KeyChar;
+            choice = checkChar(choice);
+
+            if(choice == 'y' || choice == 'Y')
+            {
+                attackSequence(player, Gorlack);
+            }
+            else
+            {
+                Console.WriteLine("You flee in terror. Game Over.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+
+        static void attackSequence(Hero player, Hero enemy)
+        {
+            Hero firstToAttack = whoGoesFirst(player, enemy);
+
+            if (firstToAttack == player)
+            {
+                attackPhase(player, enemy);
+            }
+            else
+            {
+                attackPhase(enemy, player);
+            }
+
+            if (player.Health <= 0)
+            {
+                Console.WriteLine("\nYou have died... Thanks for playing.\n");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else if (enemy.Health <= 0)
+            {
+                Console.WriteLine("\nYou have slain " + enemy.Name + "! You live to fight another day...");
+            }
+        }
+
+        static Hero whoGoesFirst(Hero player, Hero enemy)
+        {
+            Random randomInitiative = new Random();
+            float playerSpeedModifier = randomInitiative.Next(0, 11) / 10f;
+            float enemySpeedModifier = randomInitiative.Next(0, 11) / 10f;
+
+            if (player.Speed * playerSpeedModifier > enemy.Speed * enemySpeedModifier)
+            {
+                return player;
+            }
+            else
+            {
+                return enemy;
+            }
+        }
+
+        static void attackPhase(Hero first, Hero second)
+        {
+            int firstDmg;
+            int secDmg;
+            char choice;
+
+            while (first.Health > 0 && second.Health > 0)
+            {
+                Console.Clear();
+                Console.WriteLine(first.Name + " vs. " + second.Name);
+                firstDmg = first.inflictDamage(second);
+                if (firstDmg == 0)
+                {
+                    Console.WriteLine("\n" + first.Name + " missed.");
+                }
+                else
+                {
+                    Console.WriteLine("\n" + first.Name + " hits " + second.Name + " for " + firstDmg + " damage.");
+                }
+
+                secDmg = second.inflictDamage(first);
+
+                if(secDmg == 0)
+                {
+                    Console.WriteLine(second.Name + " missed.");
+                }
+                else
+                {
+                    Console.WriteLine(second.Name + " hits " + first.Name + " for " + secDmg + " damage.");
+                }
+
+                Console.WriteLine("\n" + first.Name + " Health: " + first.Health);
+                Console.WriteLine("\n" + second.Name + " Health: " + second.Health);
+
+                if(first.Health > 0 && second.Health > 0)
+                {
+                    Console.Write("\n\nContinue battle? (y/n)");
+                    choice = Console.ReadKey().KeyChar;
+
+                    choice = checkChar(choice);
+
+                    if (choice == 'n' || choice == 'N')
+                    {
+                        Console.WriteLine("\nYou have run away...\n");
+                        break;
+                    }
+                }
+            }
+        }
+
+        static char checkChar(char choice)
+        {
             while (choice != 'y' && choice != 'Y' && choice != 'n' && choice != 'N')
             {
                 Console.WriteLine();
@@ -25,29 +182,8 @@ namespace DungeonCrawler
                 choice = Console.ReadKey().KeyChar;
             }
 
-            if(choice == 'y' || choice == 'Y')
-            {
-
-            }
-            else
-            {
-                Console.WriteLine("\nWell, thanks for playing. Bye!");
-                Console.ReadKey();
-                return;
-            }
-
-            Hero player = new Hero(100, 8, 5, 3);
-            Hero enemy = new Hero(20, 4, 2, 1);
-            
-            Console.WriteLine("\nEnemy's health before Attack: " + enemy.Health);
-            player.inflictDamage(enemy);
-            Console.WriteLine("\nEnemy's health after Attack: " + enemy.Health);
-
-
-
-            Console.ReadKey();
+            return choice;
         }
 
-        
     }
 }
