c434950d64588305718863acb034c127f8cb2e93
Taylor <tbuchhei@gmail.com>
2017-05-02 20:52:22 -0700

    Controller for Climate Game Jam. Uses coroutine to change animation states based on time

---
+++ Coding with Rett/Iceberg/IcebergStateController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcebergStateController : MonoBehaviour {
+
+    Animator anim;
+    public int time;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        StartCoroutine(timeKeeper());
+	}
+
+    private IEnumerator timeKeeper()
+    {
+        for (int i = 1; i <= time; i++)
+        {
+            anim.SetInteger("Time", i);
+            yield return new WaitForSeconds(1f);
+        }
+        
+    }
+}
