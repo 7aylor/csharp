ad158ae1d80b9ef6891ee8e0d84fe31d54b019df
Taylor <tbuchhei@gmail.com>
2017-05-26 11:46:32 -0700

    unity stuff

---
+++ Coding with Rett/Game Dev's Quest/Tournament Fighter/BlackJack-DESKTOP-DU4HE4V.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament_Fighter
+{
+    public static class BlackJack
+    {
+        static List<NPC> blackJackPlayers = new List<NPC>();
+
+        static public void play()
+        {
+            blackJackPlayers.Add(GameCharacters.Norm);
+            blackJackPlayers.Add(GameCharacters.player);
+
+            //Set the dealer's (Norm) starting card position to the middle of the console window and
+            //down 4 from the top
+            GameCharacters.Norm.handStartPos.setCoords((GameConstants.WINDOW_WIDTH / 2) - 2, Console.WindowTop + 4);
+
+            //Sets the current hand position equal to the start position since we haven't dealt any cards
+            GameCharacters.Norm.handCurrPos = GameCharacters.Norm.handStartPos;
+
+            GameCharacters.player.handStartPos.setCoords((GameConstants.WINDOW_WIDTH / 2) - 2, 16);
+            GameCharacters.player.handCurrPos = GameCharacters.player.handStartPos;
+
+            deal();
+
+        }
+
+        static void deal()
+        {
+            //foreach(NPC cardPlayer in blackJackPlayers)
+            //{
+            GameCharacters.player.blackJackHand.Add(BlackJackDeck.deck.drawTopCard());
+            updatePlayerCardPos(GameCharacters.player);
+            GameCharacters.Norm.blackJackHand.Add(BlackJackDeck.deck.drawTopCard());
+            updatePlayerCardPos(GameCharacters.Norm);
+
+
+            GameCharacters.player.blackJackHand.Add(BlackJackDeck.deck.drawTopCard());
+            updatePlayerCardPos(GameCharacters.player);
+            GameCharacters.Norm.blackJackHand.Add(BlackJackDeck.deck.drawTopCard());
+            updatePlayerCardPos(GameCharacters.Norm);
+            //}
+            printNewDeal();
+        }
+
+        static void printNewDeal()
+        {
+            Helper.printCleanUI();
+
+            for(int i = 0; i < blackJackPlayers.Count; i++)
+            {
+                if (blackJackPlayers[i] == GameCharacters.Norm)
+                {
+                    string normsTitle = GameCharacters.Norm.Occupation + " - " + GameCharacters.Norm.Name;
+                    Console.SetCursorPosition((GameConstants.WINDOW_WIDTH / 2) - (normsTitle.Length / 2),
+                                               GameCharacters.Norm.handCurrPos.Y - 1);
+                    Console.WriteLine(normsTitle);
+                    Console.SetCursorPosition(GameCharacters.Norm.handCurrPos.X, GameCharacters.Norm.handCurrPos.Y);
+
+                    for (int j = 0; j < 2; j++)
+                    {
+                        if (j == 0)
+                        {
+                            GameCharacters.Norm.blackJackHand[j].printCardFaceDown();
+                        }
+                        else
+                        {
+                            GameCharacters.Norm.blackJackHand[j].printCardFaceUp();
+                        }
+                    }
+                }
+                if (blackJackPlayers[i] == GameCharacters.player)
+                {
+                    Console.SetCursorPosition((GameConstants.WINDOW_WIDTH / 2) - (GameCharacters.player.Name.Length / 2),
+                                               GameCharacters.player.handCurrPos.Y - 1);
+                    Console.WriteLine(GameCharacters.player.Name);
+                    Console.SetCursorPosition((GameConstants.WINDOW_WIDTH / 2) - 2, GameCharacters.player.handCurrPos.Y);
+
+
+                    for (int j = 0; j < 2; j++)
+                    {
+                        GameCharacters.player.blackJackHand[j].printCardFaceUp();
+                    }
+                }
+            }
+
+        }
+
+        static void updatePlayerCardPos(NPC currentPlayer)
+        {
+            //gets the last card dealt to the player
+            Card lastCard = currentPlayer.blackJackHand[currentPlayer.blackJackHand.Count - 1];
+            if(lastCard.Value == 10)
+            {
+                currentPlayer.handCurrPos.X += 3;
+            }
+            else
+            {
+                currentPlayer.handCurrPos.X += 2;
+            }
+        }
+
+        static void endGame()
+        {
+            Helper.buildPlayerNav();
+            Console.WriteLine("Press any key to flip dealer's card.");
+            Console.ReadKey();
+            Console.SetCursorPosition(GameCharacters.Norm.handStartPos.X, GameCharacters.Norm.handStartPos.Y);
+            GameCharacters.Norm.blackJackHand[0].printCardFaceUp();
+        }
+    }
+}
---
+++ Coding with Rett/Game Dev's Quest/Tournament Fighter/DeckOfCards-DESKTOP-DU4HE4V.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tournament_Fighter
+{
+    enum Suit { Spades, Hearts, Diamonds, Clubs }
+    enum Cards { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King }
+
+    class DeckOfCards
+    {
+        public List<Card> deck = new List<Card>();
+
+        /// <summary>
+        /// Default constructor. Builds a standard deck of cards
+        /// </summary>
+        public DeckOfCards()
+        {
+            buildDeck();
+        }
+
+        /// <summary>
+        /// builds a standard deck of 52 cards
+        /// </summary>
+        public void buildDeck()
+        {
+            Suit ourSuit;
+            Cards ourCard;
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 13; j++)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            ourSuit = Suit.Clubs;
+                            break;
+                        case 1:
+                            ourSuit = Suit.Diamonds;
+                            break;
+                        case 2:
+                            ourSuit = Suit.Hearts;
+                            break;
+                        default:
+                            ourSuit = Suit.Spades;
+                            break;
+                    }
+                    switch (j)
+                    {
+                        case 0:
+                            ourCard = Cards.Ace;
+                            break;
+                        case 1:
+                            ourCard = Cards.Two;
+                            break;
+                        case 2:
+                            ourCard = Cards.Three;
+                            break;
+                        case 3:
+                            ourCard = Cards.Four;
+                            break;
+                        case 4:
+                            ourCard = Cards.Five;
+                            break;
+                        case 5:
+                            ourCard = Cards.Six;
+                            break;
+                        case 6:
+                            ourCard = Cards.Seven;
+                            break;
+                        case 7:
+                            ourCard = Cards.Eight;
+                            break;
+                        case 8:
+                            ourCard = Cards.Nine;
+                            break;
+                        case 9:
+                            ourCard = Cards.Ten;
+                            break;
+                        case 10:
+                            ourCard = Cards.Jack;
+                            break;
+                        case 11:
+                            ourCard = Cards.Queen;
+                            break;
+                        default:
+                            ourCard = Cards.King;
+                            break;
+                    }//end of j switch
+                    deck.Add(new Card(ourSuit, ourCard));
+                }//end of j loop
+            }//end of i loop
+        }//end of build deck
+
+        /// <summary>
+        /// Prints the entire deck of cards
+        /// </summary>
+        public void printDeck()
+        {
+            foreach(Card card in deck)
+            {
+                card.printCardFaceUp();
+            }
+        }
+
+        /// <summary>
+        /// Draws a random card from the deck simulating a shuffled top card
+        /// Removes that card from the deck and returns it
+        /// </summary>
+        /// <returns></returns>
+        public Card drawTopCard()
+        {
+            Thread.Sleep(11);
+            Random rand = new Random();
+            int topCardIndex = rand.Next(0, deck.Count);
+            Card topCard = deck[topCardIndex];
+            deck.RemoveAt(topCardIndex);
+            return topCard;
+        }
+
+        /// <summary>
+        /// deletes the entire deck
+        /// </summary>
+        public void deleteDeck()
+        {
+            for(int i = deck.Count - 1; i >= 0; i--)
+            {
+                deck.RemoveAt(i);
+            }
+        }
+    }
+
+    class Card
+    {
+        //Keep track of x and y positions here instead of a struct
+        int cardValue;
+        Suit suit;
+        string card;
+        ConsoleColor background;
+        ConsoleColor foreground;
+
+        /// <summary>
+        /// Creates a card from the suit and card value
+        /// </summary>
+        /// <param name="suit"></param>
+        /// <param name="card"></param>
+        public Card(Suit suit, Cards card)
+        {
+            this.suit = suit;
+            
+            switch (card)
+            {
+                //Make sure to account for when ace is 1 or 11 *cardValue*
+                case Cards.Ace:
+                    this.card = "A";
+                    this.cardValue = 1;
+                    break;
+                case Cards.Two:
+                    this.card = "2";
+                    this.cardValue = 2;
+                    break;
+                case Cards.Three:
+                    this.card = "3";
+                    this.cardValue = 3;
+                    break;
+                case Cards.Four:
+                    this.card = "4";
+                    this.cardValue = 4;
+                    break;
+                case Cards.Five:
+                    this.card = "5";
+                    this.cardValue = 5;
+                    break;
+                case Cards.Six:
+                    this.card = "6";
+                    this.cardValue = 6;
+                    break;
+                case Cards.Seven:
+                    this.card = "7";
+                    this.cardValue = 7;
+                    break;
+                case Cards.Eight:
+                    this.card = "8";
+                    this.cardValue = 8;
+                    break;
+                case Cards.Nine:
+                    this.card = "9";
+                    this.cardValue = 9;
+                    break;
+                case Cards.Ten:
+                    this.card = "10";
+                    this.cardValue = 10;
+                    break;
+                case Cards.Jack:
+                    this.card = "J";
+                    this.cardValue = 11;
+                    break;
+                case Cards.Queen:
+                    this.card = "Q";
+                    this.cardValue = 12;
+                    break;
+                default:
+                    this.card = "K";
+                    this.cardValue = 13;
+                    break;
+            }
+
+            background = ConsoleColor.White;
+            if (suit == Suit.Clubs || suit == Suit.Spades)
+            {
+                foreground = ConsoleColor.Black;
+            }
+            else
+            {
+                foreground = ConsoleColor.Red;
+            }
+        }
+
+        //getters
+        public int Value
+        {
+            get
+            {
+                return this.cardValue;
+            }
+        }
+
+        /// <summary>
+        /// prints the card to the console
+        /// </summary>
+        public void printCardFaceUp()
+        {
+            char spade = '\u2660';
+            char heart = '\u2665';
+            char diamond = '\u2666';
+            char club = '\u2663';
+
+            Console.BackgroundColor = background;
+            Console.ForegroundColor = foreground;
+
+            Console.Write(this.card);
+            
+            if (this.suit == Suit.Clubs)
+            {
+                Console.Write(club);
+            }
+            else if (this.suit == Suit.Diamonds)
+            {
+                Console.Write(diamond);
+            }
+            else if (this.suit == Suit.Hearts)
+            {
+                Console.Write(heart);
+            }
+            else
+            {
+                Console.Write(spade);
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public void printCardFaceDown()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\\/");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
